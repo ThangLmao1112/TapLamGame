87b38811b79ddb362bad13eb8eccbf440642ce57
ThangLmao1112 <thang14563@gmail.com>
2025-11-14 08:25:51 +0700

    Full Lab

---
+++ Assets/Scirpts/HydrationBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HydrationBar : MonoBehaviour
+{
+    private Slider slider;
+    public TMP_Text hydrationCounter;
+
+    public GameObject playerState;
+
+    private float currentHydration, maxHydration;
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        currentHydration = playerState.GetComponent<PlayerState>().currentHydrationPrecent;
+        maxHydration = playerState.GetComponent<PlayerState>().maxHydrationPrecent;
+
+        float fillValue = currentHydration / maxHydration;
+        slider.value = fillValue;
+
+        hydrationCounter.text = currentHydration + "%";
+    }
+}
---
+++ Assets/Scirpts/InteractableObject.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InteractableObject : MonoBehaviour
+{
+    public bool playerInRange;
+
+    public string ItemName;
+
+    public string GetItemName()
+    {
+        return ItemName;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && playerInRange && SelectionManager.Instance.onTarget && SelectionManager.Instance.selectedObject==gameObject)
+        {
+            if (!InventorySystem.Instance.CheckIfFull())
+            {
+                InventorySystem.Instance.AddToInventory(ItemName);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Inventory is full!");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
